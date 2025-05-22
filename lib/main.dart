e72e5ed4437a60425049fa9860208b5c9361f0a0
Otabek Ahmedov <premierahmedov@gmail.com>
2025-05-22 14:59:20 +0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,16 +20,11 @@ class _RestaurantAppState extends State<RestaurantApp> {
   @override
   void initState() {
     super.initState();
-    // Ilova yuklanish jarayonini simulyatsiya qilish
     _initializeApp();
   }
 
   Future<void> _initializeApp() async {
-    // Bu yerda siz ilovangiz uchun kerakli ma'lumotlarni yuklashingiz mumkin.
-    // Masalan, API chaqiruvi, ma'lumotlar bazasidan o'qish va hokazo.
-    await Future.delayed(const Duration(seconds: 2)); // 2 soniya kutib turish misoli
-
-    // Ma'lumotlar yuklanib bo'lgach yoki ilova tayyor bo'lgach splash ekranini olib tashlash.
+    await Future.delayed(const Duration(seconds: 2));
     FlutterNativeSplash.remove();
   }
 
